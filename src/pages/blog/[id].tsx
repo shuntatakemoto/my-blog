045ee83d1aae226d3bb0d7a4d88da66087abb378
shuntatakemoto <rt80405@gmail.com>
2022-02-09 16:06:38 +0900

    💄 サムネイル表示とスタイル調整

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,15 +1,32 @@
-import { Badge, Typography } from '@supabase/ui';
-import { client } from '../../libs/client';
+import { Badge } from '@supabase/ui';
+import Image from 'next/image';
+import Link from 'next/link';
+import { client } from '~/libs/client';
 
 export default function BlogId({ blog }: any) {
   return (
-    <main>
-      <Typography.Title level={1}>{blog.title}</Typography.Title>
-      <Typography.Text>{blog.publishedAt}</Typography.Text>
-      <Badge color='blue' size='large'>
-        {blog.category && `${blog.category.name}`}
-      </Badge>
+    <main className='pt-8'>
+      <h1 className='py-2 font-mono text-3xl font-bold'>{blog.title}</h1>
+      <div className='flex'>
+        <p className='py-2 pr-4'>{new Date(blog.publishedAt).toLocaleDateString()}</p>
+        <Link href={`/category/${blog.category.id}`} passHref>
+          <div className='flex mx-1 mt-2 mb-8 w-16 cursor-pointer'>
+            <Badge color={blog.category.color} size='large'>
+              {blog.category && `${blog.category.name}`}
+            </Badge>
+          </div>
+        </Link>
+      </div>
+      <Image
+        key={blog.id}
+        src={`${JSON.stringify(blog.thumbnail.url).replace(/"/g, '')}`}
+        alt=''
+        width='1200'
+        height='630'
+        objectFit='cover'
+      />
       <div
+        className='pt-8'
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
         }}
