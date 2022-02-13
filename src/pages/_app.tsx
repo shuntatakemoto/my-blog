8442197756c9aaf99086fcfb14050e32ea508668
shuntatakemoto <rt80405@gmail.com>
2022-02-13 18:28:04 +0900

    📈 GA導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
 import '~/styles/global.css';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 import { Layout } from '~/components/Layout';
+import { GA_TRACKING_ID, pageview } from '~/libs/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <Layout>
       <Component {...pageProps} />
