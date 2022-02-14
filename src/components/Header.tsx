5f80f4b9947af85f4c4811271225fa820493f054
shuntatakemoto <rt80405@gmail.com>
2022-02-14 18:01:12 +0900

    💄 リンクテキストに下線を追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Image from 'next/image';
 import Link from 'next/link';
 import type { VFC } from 'react';
@@ -5,9 +6,12 @@ import type { VFC } from 'react';
 export const Header: VFC = () => {
   return (
     <header className='flex justify-between items-center py-8 bg-gray-100'>
-      <h1 className='font-header text-3xl font-bold'>
-        <Link href='/'>Take It Easy!</Link>
-      </h1>
+      <Link href='/' passHref>
+        <a rel='noopener noreferrer' className='no-underline'>
+          <h1 className='font-header text-3xl font-bold'>Take It Easy!</h1>
+        </a>
+      </Link>
+
       <a href='https://twitter.com/haruta_8_' target='_blank' rel='noopener noreferrer'>
         <Image src='/haruta.png' alt='haruta-image' width='72' height='72' quality='10' />
       </a>
